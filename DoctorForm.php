fbcfba1cecf2a42a36971e46cd6d4d3e36bc475b
Ellahi171 <103473484+Ellahi171@users.noreply.github.com>
2022-04-12 11:21:44 -0700

    This is a php file of form for the doctor

---
+++ DoctorForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Field;
+use Kris\LaravelFormBuilder\Form;
+
+class DoctorForm extends Form
+{
+    public function buildForm()
+    {
+        $user = $this->getData('user');
+
+        if(isset($user)){
+            $this->add('doctor_id','hidden',[
+                'value' => $user->id
+            ]);
+        }
+
+        $this->add('email', Field::EMAIL,[
+            'rules' => 'required|string',
+            'value' => @$user->email
+        ])->add('password', Field::PASSWORD,[
+            'rules' => 'required',
+        ])->add('name', Field::TEXT,[
+            'rules' => 'required|string|regex:/^[a-zA-Z]+$/u',
+            'value' => @$user->email
+        ])->add('phone_no', Field::NUMBER,[
+            'rules' => 'required|numeric',
+            'value' => @$user->phone_no,
+        ])->add('address', Field::TEXT,[
+            'rules' => 'required|string',
+            'value' => @$user->address
+        ])->add('date_of_birth', Field::DATE,[
+            'rules' => 'required',
+            'value' => @$user->date_of_birth
+        ])->add('degree', Field::TEXT,[
+            'rules' => 'required||regex:/^[a-zA-Z]+$/u|string',
+            'value' => @$user->degree
+        ])->add('specialty', Field::TEXT,[
+            'rules' => 'required||regex:/^[a-zA-Z]+$/u|string',
+            'value' => @$user->specialty
+        ])->add('submit', 'submit', [
+            'label' => isset($user)?'Update':'Create',
+            'attr' => ['class' => 'btn btn-danger'],
+        ]);
+    }
+}
