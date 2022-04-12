5c1e8cc41aa4a2fe23663fc45244d5e0ea6b05d3
Ellahi171 <103473484+Ellahi171@users.noreply.github.com>
2022-04-12 11:20:20 -0700

    This is a php file for user registration form

---
+++ ProfileForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Field;
+use Kris\LaravelFormBuilder\Form;
+
+class ProfileForm extends Form
+{
+    public function buildForm()
+    {
+        $user = $this->getData('user');
+
+        $this->add('name', Field::TEXT,[
+            'rules' => 'required',
+            'value' => @$user->name
+        ])->add('email', Field::TEXT,[
+            'rules' => 'required',
+            'value' => @$user->email
+        ])->add('password', Field::PASSWORD,[
+            'rules' => 'required',
+        ])->add('submit', 'submit', [
+            'label' => 'Update',
+            'attr' => ['class' => 'btn btn-danger'],
+        ]);
+    }
+}
